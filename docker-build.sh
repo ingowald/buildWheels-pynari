213445b79786ae7177fd7c7288414a7e681ccfa7
Ingo Wald <ingowald@gmail.com>
2024-12-08 14:34:30 -0700

    added on readme, updated barney, exposed more module-level constants in init.py

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 alias ll="ls -lisa"
-export OLD_PATH=$PATH
-export PATH=/opt/python/cp312-cp312/bin/:$OLD_PATH
 
 yum -y install gcc-toolset-13
 source /opt/rh/gcc-toolset-13/enable
-
 
 echo "== installing cuda toolkit"
 chmod +x ./cuda_12.6.3_560.35.05_linux.run
@@ -13,11 +10,13 @@ chmod +x ./cuda_12.6.3_560.35.05_linux.run
 export PATH=/usr/local/cuda/bin/:$PATH
 #export PATH=/usr/local/cuda-12.6/bin//:$PATH
 
+export OLD_PATH=$PATH
 cd /io
 for f in cp38 cp39 cp310 cp311 cp312 cp313 ; do
-    pip3 install scikit-build wheel twine
     echo "== building wheel for $f"
     export PATH=/opt/python/$f-$f/bin/:$OLD_PATH
+    #export PATH=/opt/python/cp312-cp312/bin/:$OLD_PATH
+    pip3 install scikit-build wheel twine
     python3 setup.py bdist_wheel
 done
 
